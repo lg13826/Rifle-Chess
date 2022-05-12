fdb4b4ba37de632c3f631eeb27a5ae461b3458b6
lg13826 <47194138+lg13826@users.noreply.github.com>
2022-05-11 21:40:58 -0700

    Screen shows, still need to draw

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from curses.ascii import isupper
 from piece import GamePiece
 
 def square_position_equation(row, column, row_length):
@@ -19,38 +20,60 @@ class Board:
     def __init__(self):
         self._square = [x for x in range(int(64))]
         self._board_dimension = 8
+        self._square[0] = 'r'
+        self._square[1] = 'n'
+        self._square[2] = 'b'
+        self._square[3] = 'q'
+        self._square[4] = 'k'
+        self._square[5] = 'b'
+        self._square[6] = 'n'
+        self._square[7] = 'r'
+        self._square[len(self._square)] = 'R'
+        self._square[len(self._square) - 1] = 'N'
+        self._square[len(self._square) - 2] = 'B'
+        self._square[len(self._square) - 3] = 'Q'
+        self._square[len(self._square) - 4] = 'K'
+        self._square[len(self._square) - 5] = 'B'
+        self._square[len(self._square) - 6] = 'N'
+        self._square[len(self._square) - 7] = 'R'
+        for index in range(8, 16):
+            self._square[index] = 'p'
+        for index in range(len(self._square) - 16, 8 - len(self._square)):
+            self._square[index] = 'P'
 
-    def squares(self):
-        return self._square
+    def is_piece_white(self, index):
+        return isupper(self._square[index])
 
-    def fen_load(self, fen_string, fen_dict = fen_dict):
-        #parse through string
-        row = 0
-        for i in range(len(fen_string)):
-            #check for line break
-            if (fen_string[i] == '/'):
-                        break;
-            #check for number
-            if (fen_string[i].isnumeric()):
-                
-                continue
-            #check for letter
-            if 
-                
-            for j in range(self._board_dimension):
-                for k in range(self._board_dimension):
-                    
-                    elif fen_string[i] in fen_dict:
-                        if (fen_string[i].isupper()):
-                            self._square[square_position_equation(k, j, self._board_dimension)] = fen_dict[fen_string[i]][0]
-                            break;
-                        else:
-                            self._square[square_position_equation(k, j, self._board_dimension)] = fen_dict[fen_string[i]][1]
-                            break;
+    def square_index(self, index):
+        return self._square[index]
 
-    def board_dimension(self):
-        return self._board_dimension
+#    def fen_load(self, fen_string, fen_dict = fen_dict):
+#        #parse through string
+#        row = 0
+#        for i in range(len(fen_string)):
+#            #check for line break
+#            if (fen_string[i] == '/'):
+#                        break;
+#            #check for number
+#            if (fen_string[i].isnumeric()):
+#                
+#                continue
+#            #check for letter
+#            if 
+#                
+#            for j in range(self._board_dimension):
+#                for k in range(self._board_dimension):
+#                    
+#                    elif fen_string[i] in fen_dict:
+#                        if (fen_string[i].isupper()):
+#                            self._square[square_position_equation(k, j, self._board_dimension)] = fen_dict[fen_string[i]][0]
+#                            break;
+#                        else:
+#                            self._square[square_position_equation(k, j, self._board_dimension)] = fen_dict[fen_string[i]][1]
+#                            break;
+#
+#    def board_dimension(self):
+#        return self._board_dimension
 
 if __name__ == "__main__":
     b = Board()
-    b.fen_load("7k/3R")
--- game.py
+++ game.py
@@ -1,1 +1,30 @@
-#empty
+import pygame
+from pygame.locals import *
+ 
+
+class Game:
+    
+    def __init__(self):
+        pygame.init()
+        vec = pygame.math.Vector2  # 2 for two dimensional
+        self._height = 800
+        self._width = 800
+        self._square = []
+        for i in range(63):
+            row_pixel = 0
+            column_pixel = 0
+            #use rect.draw instead
+            self._square.append(pygame.Rect(row_pixel, column_pixel, self._width//8, self._height//8))
+            self._square[i].fill
+            if (row_pixel > self._width - 1):
+                row_pixel += self._width//8
+            else: 
+                column_pixel += self._height//8
+                row_pixel = 0
+        
+        self._displaysurface = pygame.display.set_mode((self._width, self._height))
+        pygame.display.set_caption("Rifle Chess")
+    
+    
+    
+
